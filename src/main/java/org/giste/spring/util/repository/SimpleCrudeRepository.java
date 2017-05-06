f4e369f08b9bf56fd5ae460dd07cd9f07bcc777a
giste <giste.trappiste@gmail.com>
2017-05-06 09:45:31 +0200

    Moved components depending on Spring server to a new project.

--- src/main/java/org/giste/spring/util/repository/SimpleCrudeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.giste.spring.util.repository;
-
-import org.giste.spring.util.entity.SimpleNonRemovableEntity;
-
-public interface SimpleCrudeRepository extends CrudeRepository<SimpleNonRemovableEntity> {
-
-}
--- src/main/java/org/giste/spring/util/service/SimpleCrudeService.java
+++
@@ -1,7 +0,0 @@
-package org.giste.spring.util.service;
-
-import org.giste.spring.util.dto.SimpleNonRemovableDto;
-
-public interface SimpleCrudeService extends CrudeService<SimpleNonRemovableDto> {
-
-}
